20eb5301f77b8066845e7d895e61ca3c1d42ca0c
ratan <ratanchakma68@gmail.com>
2024-05-09 08:30:42 +0600

    register,login,navbar and error page added

--- src/Route/Route.jsx
+++ src/Route/Route.jsx
@@ -1,17 +1,28 @@
 import Main from '../layout/Main.jsx';
 import Home from '../pages/Home/Home/Home.jsx';
 import {createBrowserRouter} from "react-router-dom";
+import Login from '../pages/Login/Login.jsx';
+import Register from '../pages/Register/Register.jsx';
+import Error from '../pages/Error/Error.jsx';
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Main />,
-        // element: <ErrorElement />,
+        element: <Error/>,
         children: [
             {
                 path: "/",
                 element: <Home />,
             },
+            {
+                path:"/login",
+                element:<Login/>
+            },
+            {
+                path:"/registration",
+                element:<Register/>
+            }
            
         ],
     },
